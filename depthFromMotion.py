8875bcfe07795e026ab05c5c1988dedb2faf92e4
twetto <franky85@hotmail.com.tw>
2020-11-02 21:39:29 +0800

    Discard interpolation along time axis

--- depthFromMotion.py
+++ depthFromMotion.py
@@ -43,9 +43,9 @@ def depth_perception(wall, K, Rt_rotate, Rt_translate, flow, noRotateFlow, depth
 
     return depth
 
-pose = open('201010/pose.txt', 'r')
+pose = open('201102/pose.txt', 'r')
 wall = np.loadtxt('wall.txt')
-cap = cv2.VideoCapture("201010/2020-10-10_12-41-29.mkv")
+cap = cv2.VideoCapture("201102/video.mkv")
 width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
 width = int(width)
 height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
@@ -60,18 +60,11 @@ writer_nrflow = cv2.VideoWriter("output_noRotateFlow.mkv", cv2.VideoWriter_fourc
 #writer_outlier = cv2.VideoWriter("output_outlier.mkv", cv2.VideoWriter_fourcc(*'X264'),fps,(640,480))
 writer_depth = cv2.VideoWriter("output_depth.mkv", cv2.VideoWriter_fourcc(*'MJPG'),fps,(width,height))
 
-video_skip = int(5.75*fps)
-pose_skip = 183.406
-
 dis = cv2.DISOpticalFlow_create(0)
 
-K = np.array([[focalLength, 0.0, width/2],
-             [0.0, focalLength, height/2],
+K = np.array([[focalLength, 0.0, width/2+0.5],
+             [0.0, focalLength, height/2+0.5],
              [0.0, 0.0, 1.0]])
-
-# skip unwanted frames
-for i in range(video_skip):
-    ret, frame = cap.read()
 
 ret, frame = cap.read()
 hsv = np.zeros_like(frame)
@@ -80,137 +73,113 @@ hsv[...,1] = 255
 curr = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 prev = curr.copy()
 
-# pf = prev frame, pp = prev pose
-pftime, pftx, pfty, pftz, pfrx, pfry = pose_skip, -13.21110, 4.00000, 77.31789, np.radians(-2.70001), np.radians(3.30591)
-pptime, pptx, ppty, pptz, pprx, ppry = pose_skip, -13.21110, 4.00000, 77.31789, np.radians(-2.70001), np.radians(3.30591)
+ptime, ptx, pty, ptz, prx, pry = 261.03900, 785.56394, 4.00000, -206.83414, np.radians(-1.35001), np.radians(176.10004)
 
-frame_count = 0
 start = timer()
+frame_count = 0
 time_flow = 0
 time_rt = 0
 time_depth = 0
 for line in pose:
     time, tx_, ty_, tz_, rx_, ry_ = line.split()
     time = float(time)
-    time -= pose_skip
     
-    # skip unwanted poses
-    if(time < 0): continue
-
     tx, ty, tz, rx, ry = float(tx_), float(ty_), float(tz_), np.radians(float(rx_)), np.radians(float(ry_))
     
-    interpolated = True
-    while(interpolated):
+    # calculate flow
+    start_flow = timer()
+    diff = cv2.absdiff(prev, curr)
+    flow = dis.calc(prev, curr, None, )
+    time_flow += timer() - start_flow
+    #showFrame = frame.copy()
+    noRotateFlow = np.zeros_like(flow)
+    depth = np.zeros_like(curr).astype(np.float32)
+    depth += 480
+    #outlier = np.zeros_like(curr).astype(np.float32)
+    
+    start_rt = timer()
+    # rotation in radius
+    Rh = np.array([[1,            0,             0],
+                  [ 0, np.cos(prx), -np.sin(prx)],
+                  [ 0, np.sin(prx),  np.cos(prx)]])
+    Ry = np.array([[np.cos(ry-pry), 0, np.sin(ry-pry)],
+                  [          0,      1,               0],
+                  [-np.sin(ry-pry), 0, np.cos(ry-pry)]])
+    Rx = np.array([[1,           0,            0],
+                  [ 0, np.cos(-rx), -np.sin(-rx)],
+                  [ 0, np.sin(-rx),  np.cos(-rx)]])
+    RI = np.eye(3, 3)
 
-        # if no need to interpolate then just use current pose
-        if(abs(time - frame_count/fps) < 0.0025):
-            itime, itx, ity, itz, irx, iry = time, tx, ty, tz, rx, ry
-            interpolated = False
-        # interpolate pose when video and pose timestamp differ too much
-        elif((pptime - frame_count/fps) < 0 and (time - frame_count/fps) > 0):
-            itime, itx, ity, itz, irx, iry = (pptime+time)/2, (pptx+tx)/2, (ppty+ty)/2, (pptz+tz)/2, (pprx+rx)/2, (ppry+ry)/2
-            # interpolated = True
-        # skip misaligned poses
-        else:
-            pptime, pptx, ppty, pptz, pprx, ppry = time, tx, ty, tz, rx, ry
-            # interpolated = False
-            break
+    # translation in pixels (1 meter = 16 pixels in Minecraft)
+    xd, yd, zd = tx-ptx, ty-pty, tz-ptz
+    xd *= 16
+    yd *= 16
+    zd *= 16
+    zd = -zd
+    translation = [np.sqrt(xd**2+yd**2+zd**2)*np.sin(np.arctan2(np.sqrt(xd**2+zd**2),yd)-rx)*np.cos(np.pi/2+np.arctan2(-xd,zd)+ry),
+            np.sqrt(xd**2+yd**2+zd**2)*np.cos(np.arctan2(np.sqrt(xd**2+zd**2),yd)-rx),
+            np.sqrt(xd**2+yd**2+zd**2)*np.sin(np.arctan2(np.sqrt(xd**2+zd**2),yd)-rx)*np.sin(np.pi/2+np.arctan2(-xd,zd)+ry)]
+    translation = np.array(translation).reshape(3, 1)
+    translation_O = [0, 0, 0]
+    translation_O = np.array(translation_O).reshape(3, 1)
 
-        # calculate flow
-        start_flow = timer()
-        diff = cv2.absdiff(prev, curr)
-        flow = dis.calc(prev, curr, None, )
-        time_flow += timer() - start_flow
-        #showFrame = frame.copy()
-        noRotateFlow = np.zeros_like(flow)
-        depth = np.zeros_like(curr).astype(np.float32)
-        depth += 480
-        #outlier = np.zeros_like(curr).astype(np.float32)
-        
-        start_rt = timer()
-        # rotation in radius
-        Rh = np.array([[1,            0,             0],
-                      [ 0, np.cos(pfrx), -np.sin(pfrx)],
-                      [ 0, np.sin(pfrx),  np.cos(pfrx)]])
-        Ry = np.array([[np.cos(iry-pfry), 0, np.sin(iry-pfry)],
-                      [          0,      1,               0],
-                      [-np.sin(iry-pfry), 0, np.cos(iry-pfry)]])
-        Rx = np.array([[1,           0,            0],
-                      [ 0, np.cos(-irx), -np.sin(-irx)],
-                      [ 0, np.sin(-irx),  np.cos(-irx)]])
-        RI = np.eye(3, 3)
+    # Rt = [R|t]
+    Rt_rotate = np.concatenate((Rh @ Ry @ Rx, translation_O), axis=1)
+    Rt_translate = np.concatenate((RI, translation), axis=1)
+    time_rt += timer() - start_rt
+    
+    # get depth frame
+    start_depth = timer()
+    depth = depth_perception(wall, K, Rt_rotate, Rt_translate, flow, noRotateFlow, depth, zd, diff)
+    time_depth += timer() - start_depth
 
-        # translation in pixels (1 meter = 16 pixels in Minecraft)
-        xd, yd, zd = itx-pftx, ity-pfty, itz-pftz
-        xd *= 16
-        yd *= 16
-        zd *= 16
-        zd = -zd
-        translation = [np.sqrt(xd**2+yd**2+zd**2)*np.sin(np.arctan2(np.sqrt(xd**2+zd**2),yd)-irx)*np.cos(np.pi/2+np.arctan2(-xd,zd)+iry),
-                np.sqrt(xd**2+yd**2+zd**2)*np.cos(np.arctan2(np.sqrt(xd**2+zd**2),yd)-irx),
-                np.sqrt(xd**2+yd**2+zd**2)*np.sin(np.arctan2(np.sqrt(xd**2+zd**2),yd)-irx)*np.sin(np.pi/2+np.arctan2(-xd,zd)+iry)]
-        translation = np.array(translation).reshape(3, 1)
-        translation_O = [0, 0, 0]
-        translation_O = np.array(translation_O).reshape(3, 1)
+    # some outputs
+    mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+    hsv[...,0] = ang*180/np.pi/2
+    hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+    #hsv[...,2] = 6 * mag
+    bgr = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+    cv2.imshow('Flow', bgr)
+    writer_flow.write(bgr)
 
-        # Rt = [R|t]
-        Rt_rotate = np.concatenate((Rh @ Ry @ Rx, translation_O), axis=1)
-        Rt_translate = np.concatenate((RI, translation), axis=1)
-        time_rt += timer() - start_rt
-        
-        # get depth frame
-        start_depth = timer()
-        depth = depth_perception(wall, K, Rt_rotate, Rt_translate, flow, noRotateFlow, depth, zd, diff)
-        time_depth += timer() - start_depth
+    mag, ang = cv2.cartToPolar(noRotateFlow[...,0], noRotateFlow[...,1])
+    hsv[...,0] = ang*180/np.pi/2
+    hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+    #hsv[...,2] = 6 * mag
+    bgr = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+    cv2.imshow('nrFlow', bgr)
+    writer_nrflow.write(bgr)
 
-        # some outputs
-        mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-        hsv[...,0] = ang*180/np.pi/2
-        hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-        #hsv[...,2] = 6 * mag
-        bgr = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
-        cv2.imshow('Flow', bgr)
-        writer_flow.write(bgr)
+    cv2.imshow("frame", frame)
+    writer.write(frame)
 
-        mag, ang = cv2.cartToPolar(noRotateFlow[...,0], noRotateFlow[...,1])
-        hsv[...,0] = ang*180/np.pi/2
-        hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-        #hsv[...,2] = 6 * mag
-        bgr = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
-        cv2.imshow('nrFlow', bgr)
-        writer_nrflow.write(bgr)
-
-        cv2.imshow("frame", frame)
-        writer.write(frame)
-
-        depth *= 255 / 480
-        depth = 255 - depth
-        depth = depth * (depth > 0)
-        depth = depth.astype(np.uint8)
-        depth = cv2.applyColorMap(depth, cmapy.cmap('viridis'))
-        cv2.imshow("depth", depth)
-        writer_depth.write(depth)
-        
-        '''
-        #outlier = cv2.normalize(outlier,None,0,255,cv2.NORM_MINMAX)
-        outlier *= 255 / np.pi
-        outlier = outlier.astype(np.uint8)
-        cv2.imshow("outlier", outlier)
-        outlier = cv2.cvtColor(outlier, cv2.COLOR_GRAY2BGR)
-        writer_outlier.write(outlier)
-        '''
-        
-        prev = curr
-        pptime, pptx, ppty, pptz, pprx, ppry = itime, itx, ity, itz, irx, iry
-        pftime, pftx, pfty, pftz, pfrx, pfry = itime, itx, ity, itz, irx, iry
-        cv2.waitKey(1)
-        ret, frame = cap.read()
-        frame_count += 1
-        end = timer()
-        if(frame_count % fps == 0): print(1/(end - start))
-        start = end
-        if(ret): curr = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        else: print("Something went wrong with the video...")
+    depth *= 255 / 480
+    depth = 255 - depth
+    depth = depth * (depth > 0)
+    depth = depth.astype(np.uint8)
+    depth = cv2.applyColorMap(depth, cmapy.cmap('viridis'))
+    cv2.imshow("depth", depth)
+    writer_depth.write(depth)
+    
+    '''
+    #outlier = cv2.normalize(outlier,None,0,255,cv2.NORM_MINMAX)
+    outlier *= 255 / np.pi
+    outlier = outlier.astype(np.uint8)
+    cv2.imshow("outlier", outlier)
+    outlier = cv2.cvtColor(outlier, cv2.COLOR_GRAY2BGR)
+    writer_outlier.write(outlier)
+    '''
+    
+    prev = curr
+    ptime, ptx, pty, ptz, prx, pry = time, tx, ty, tz, rx, ry
+    cv2.waitKey(1)
+    ret, frame = cap.read()
+    end = timer()
+    frame_count += 1
+    if(frame_count % fps == 0): print(1/(end - start))
+    start = end
+    if(ret): curr = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    else: print("Something went wrong with the video...")
 
 cap.release()
 writer.release()
