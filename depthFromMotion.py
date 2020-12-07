28e530abcce4e57b40d135b0e4d43f4e123e36c8
BuildTools <unconfigured@null.spigotmc.org>
2020-12-07 22:28:14 +0800

    Add floor filtering function

--- depthFromMotion.py
+++ depthFromMotion.py
@@ -39,6 +39,73 @@ def depth_perception(wall, K, Rt_rotate, Rt_translate, flow, noRotateFlow, depth
 
     return depth, noRotateFlow
 
+def no_floor(depth, K, translation, rx, dfloor, srange, width, height):
+    if (LA.norm(translation) > 0):
+        
+        # create floor from known distance and fixed sensing range
+        translation = translation.flatten()
+        n1 = translation / LA.norm(translation)
+        origin = R.from_euler('x', rx).as_matrix() @ np.array([0, dfloor, 0])
+
+        # make unit vector on XZ plane and perpendicular to translation vector
+        not_n1 = R.from_euler('x', rx).as_matrix() @ np.array([0, 1, 0])
+        if((abs(n1) == not_n1).all()):
+            not_n1 = np.array([1.0, 0, 0])
+        n2 = np.cross(n1, not_n1)
+        n2 /= LA.norm(n2)
+
+        # rotate n1 and n2 to align with YZ plane
+        n3 = np.cross(n1, n2)
+        n4 = np.cross(n3, np.array([1.0, 0, 0]))
+        n4 /= LA.norm(n4)
+        theta = np.arccos(np.clip(np.dot(n1, n4), -1.0, 1.0))
+        if(n1[0] < 0): theta *= -1
+        quat = np.array([np.sin(theta/2)*n3[0], np.sin(theta/2)*n3[1], np.sin(theta/2)*n3[2], np.cos(theta/2)])
+        r = R.from_quat(quat).as_matrix()
+        n1 = r @ n1
+        n2 = r @ n2
+
+        # create meshgrid
+        meshx, meshz = 2, 5
+        rectx = np.linspace(-srange, srange, meshx)
+        rectz = np.linspace(0, srange, meshz)
+        rectx, rectz = np.meshgrid(rectx, rectz)
+        mx = srange / (meshx-1)
+        mz = srange / (meshz-1) / 2
+
+        # generate coordinates for floor
+        X, Y, Z = [origin[i] + rectz * n1[i] + rectx * n2[i] for i in [0, 1, 2]]
+        X, Y, Z = X.flatten(), Y.flatten(), Z.flatten()
+        X = np.delete(X, np.where(Z <= 0))
+        Y = np.delete(Y, np.where(Z <= 0))
+        Z = np.delete(Z, np.where(Z <= 0))
+        
+        # floor masking
+        masks = np.zeros_like(depth, dtype=np.uint8)
+        twenty = masks + 20
+        for x, y, z in zip(X, Y, Z):
+            p = np.array([x, y, z])
+            mask = np.array([p-n1*mz-n2*mx, p-n1*mz+n2*mx, p+n1*mz+n2*mx, p+n1*mz-n2*mx])
+            if((mask[:,2] < 0).any()): continue
+            u, v, w = K @ mask.T
+            u /= w
+            v /= w
+            w = 1.0
+            pts = np.array([u, v], dtype=np.int).T
+            l, r, t, b = np.min(pts[:,0]), np.max(pts[:,0]), np.min(pts[:,1]), np.max(pts[:,1])
+            l, r = np.clip(np.array([l, r]), 0, width)
+            t, b = np.clip(np.array([t, b]), 0, height)
+            if(l == r or t == b): continue
+            sub_depth = np.abs(depth[t:b,l:r] - z) < 0.7
+            masks[t:b,l:r] = sub_depth
+        masks_inv = ~masks
+        depth *= masks_inv > 0
+        twenty *= masks > 0
+        depth = depth + twenty
+    
+    return depth
+
+
 pose = open('201105/pose.txt', 'r')
 wall = np.loadtxt('wall.txt')
 cap = cv2.VideoCapture("201105/video.mkv")
@@ -70,11 +137,16 @@ prev = curr.copy()
 
 ptime, ptx, pty, ptz, prx, pry = 85.20800, -12.95121, 79.00000, 104.45334, np.radians(43.20006), np.radians(-145.04961)
 
+srange = 3
+dfloor = 1.6
+
 start = timer()
 frame_count = 0
 time_flow = 0
 time_rt = 0
 time_depth = 0
+time_contour = 0
+
 for line in pose:
     time, tx_, ty_, tz_, rx_, ry_ = line.split()
     time = float(time)
@@ -86,14 +158,12 @@ for line in pose:
     diff = cv2.absdiff(prev, curr)
     flow = dis.calc(prev, curr, None, )
     time_flow += timer() - start_flow
-    #showFrame = frame.copy()
     noRotateFlow = np.zeros_like(flow)
     depth = np.zeros_like(curr).astype(np.float32)
     depth += 20
-    #outlier = np.zeros_like(curr).astype(np.float32)
     
     start_rt = timer()
-    # rotation in radius
+    # rotation in radians
     Rh = np.array([[1,            0,             0],
                   [ 0, np.cos(rx), -np.sin(rx)],
                   [ 0, np.sin(rx),  np.cos(rx)]])
@@ -105,7 +175,7 @@ for line in pose:
                   [ 0, np.sin(-prx),  np.cos(-prx)]])
     RI = np.eye(3, 3)
 
-    # translation in meters
+    # translation from world frame to camera frame
     xd, yd, zd = tx-ptx, ty-pty, tz-ptz
     xd = -xd
     yd = -yd
@@ -121,8 +191,13 @@ for line in pose:
     
     # get depth frame
     start_depth = timer()
-    depth, noRotateFlow = depth_perception(wall, K, Rt_rotate, Rt_translate, flow, noRotateFlow, depth, zd, diff)
+    depth, noRotateFlow = depth_perception(wall, K, Rt_rotate, Rt_translate, flow, noRotateFlow, depth, translation.flatten()[2], diff)
     time_depth += timer() - start_depth
+
+    # filter out floor
+    start_contour = timer()
+    fdepth = no_floor(depth, K, translation, rx, dfloor, srange, width, height)
+    time_contour += timer() - start_contour
 
     # some outputs
     mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
@@ -151,6 +226,13 @@ for line in pose:
     depth = cv2.applyColorMap(depth, cmapy.cmap('viridis'))
     cv2.imshow("depth", depth)
     #writer_depth.write(depth)
+    
+    fdepth *= 255 / 20
+    fdepth = 255 - fdepth
+    fdepth = fdepth * (fdepth > 0)
+    fdepth = fdepth.astype(np.uint8)
+    fdepth = cv2.applyColorMap(fdepth, cmapy.cmap('viridis'))
+    cv2.imshow("fdepth", fdepth)
     
     '''
     #outlier = cv2.normalize(outlier,None,0,255,cv2.NORM_MINMAX)
@@ -192,3 +274,5 @@ print("rigid transform time:")
 print(time_rt)
 print("compensation & depth time:")
 print(time_depth)
+print("contouring time:")
+print(time_contour)
