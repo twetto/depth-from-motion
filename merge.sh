e9216e9499f10f7ecfa808bc34ae69f4fa230b3c
BuildTools <unconfigured@null.spigotmc.org>
2020-12-07 22:37:57 +0800

    Minor change

--- merge.sh
+++ merge.sh
@@ -1,5 +1,5 @@
 rm compare.mkv
 ffmpeg -i output.mkv -i output_flow.mkv -filter_complex hstack -c:v libx264 -crf 18 up.mkv
-ffmpeg -i output_noRotateFlow.mkv -i output_depth.mkv -filter_complex hstack -c:v libx264 -crf 18 down.mkv
+ffmpeg -i output_depth.mkv -i output_obs.mkv -filter_complex hstack -c:v libx264 -crf 18 down.mkv
 ffmpeg -i up.mkv -i down.mkv -filter_complex vstack -c:v libx264 -crf 18 compare.mkv
 rm up.mkv down.mkv
